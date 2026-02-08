38328a851918e569f8f2095437b217fcf94e4233
Abigail Martin <3010468m@student.gla.ac.uk>
2026-02-08 19:23:26 +0000

    categories view added

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
 
 from rango.models import Category
+from rango.models import Page
 
 def index(request):
     # query database for list of all cats currently stored
@@ -20,3 +20,27 @@ def index(request):
 
 def about(request):
     return render(request, 'rango/about.html', {'title': 'About'})
+
+def show_category(request, category_name_slug):
+    # create context dict to pass to template rendering engine
+    context_dict = {}
+
+    try:
+        #try find a cat name slug with given name
+        # if cant, the .get() method raises exception
+        # the .get() method returns one mode instance or raises exception
+        category = Category.objects.get(slug=category_name_slug)
+
+        # retrieve all associated pages
+        pages = Page.objects.filter(category=category)
+
+        #add results list to template context under name pages
+        context_dict['pages'] = pages
+        # also add cat obj from database to context dict
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        # if no specified cat found, dont do anything
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context=context_dict)
