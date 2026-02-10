1e827725551607f2e655a8f8af09104aba88a06e
Abigail Martin <3010468m@student.gla.ac.uk>
2026-02-10 23:28:30 +0000

    categories template added

--- rango/views.py
+++ rango/views.py
@@ -25,7 +25,9 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return render(request, 'rango/about.html', {'title': 'About'})
+    print(request.method)
+    print(request.user)
+    return render(request, 'rango/about.html', {})
 
 def show_category(request, category_name_slug):
     # create context dict to pass to template rendering engine
