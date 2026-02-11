c748af29b5dc1c3cb3c83267b47f0649bd6f8bb0
Abigail Martin <3010468m@student.gla.ac.uk>
2026-02-11 15:58:15 +0000

    chap9 complete

--- rango/views.py
+++ rango/views.py
@@ -56,6 +56,7 @@ def show_category(request, category_name_slug):
 
     return render(request, 'rango/category.html', context=context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
 
@@ -76,6 +77,7 @@ def add_category(request):
     # handles bad form, new form or no form cases
     return render(request, 'rango/add_category.html', {'form': form})
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -159,10 +161,11 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html')
 
 @login_required
 def user_logout(request):
     # we know user is logged in, so can just log them out
     logout(request)
     return redirect(reverse('rango:index'))
+
